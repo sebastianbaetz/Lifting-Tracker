359b91e850168ed17af45536619e79f1c14940c9
Sebastián Javier Baetz <s128baetz@gmail.com>
2022-06-26 14:45:37 -0500

    Initial commit

---
+++ src/test/java/com/springwebapp/liftingtracker/LiftingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebapp.liftingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftingTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
